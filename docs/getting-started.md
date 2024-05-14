78ced29e233df23cc7660b56194d2c4068d33829
Kashish Shah <kashishshah2205@gmail.com>
2024-05-14 21:19:51 +0530

    initial setup

---
+++ docs/getting-started.md
@@ -0,0 +1,9 @@
+---
+id: getting-started
+title: "Getting Started"
+description: Introduction to Lagrange
+author: kashish
+content_type: quickstart
+---
+
+Lagrange State Committees and Lagrange Hyper-Parallel ZK Coprocessor
---
+++ sidebars.ts
@@ -0,0 +1,13 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebars: SidebarsConfig = {
+  docsSidebar: [
+    {
+      type: "doc",
+      label: "Getting Started",
+      id: "getting-started",
+    },
+  ],
+};
+
+export default sidebars;
