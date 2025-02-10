d95d2002d51f1ef57fc29a9c834031d592a61e28
Kashish Shah <kashish@lagrange.dev>
2025-02-10 22:56:27 +0530

    chore: remove LSC links from home and navbar (#50)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,14 +12,19 @@ export default function Home() {
           Welcome to the Lagrange documentation!
         </p>
         <p className={styles.overview}>
-        Welcome to the technical documentation and knowledge resources for Lagrange, the infinite proving layer. Lagrange consists of 3 protocols - a decentralized ZK Prover Network, a hyper-parallel ZK Coprocessor and State Committees for interoperability. This documentation shares the concepts, architecture, and deployment guides for each of Lagrange’s protocols.
+          Welcome to the technical documentation and knowledge resources for
+          Lagrange, the infinite proving layer. Lagrange consists of two
+          protocols - a decentralized ZK Prover Network and a hyper-parallel ZK
+          Coprocessor. This documentation shares the concepts, architecture, and
+          other relevant information for each of Lagrange’s protocols.
         </p>
         <div className={styles.cards}>
-        <Link to="/prover-network/overview" className={styles.card}>
+          <Link to="/prover-network/overview" className={styles.card}>
             <div className={styles.cardContent}>
               <h3 className={styles.cardTitle}>ZK Prover Network</h3>
               <p>
-              Lagrange’s ZK Prover Network supports universal proving - for rollups, apps, coprocessors and interoperability.  
+                Lagrange’s ZK Prover Network supports universal proving - for
+                rollups, apps, coprocessors and interoperability.
               </p>
             </div>
           </Link>
@@ -27,20 +32,11 @@ export default function Home() {
             <div className={styles.cardContent}>
               <h3 className={styles.cardTitle}>ZK Coprocessor</h3>
               <p>
-              The ZK Coprocessor enables developers to prove custom SQL queries over onchain data, directly from smart contracts.
+                The ZK Coprocessor enables developers to prove custom SQL
+                queries over onchain data, directly from smart contracts.
               </p>
             </div>
           </Link>
-          <Link to="/state-committees/overview" className={styles.card}>
-            <div className={styles.cardContent}>
-              <h3 className={styles.cardTitle}>State Committees</h3>
-              <p>
-                State Committees are fast-finality ZK state proofs that enable
-                bridging and interoperability between optimistic rollups.
-              </p>
-            </div>
-          </Link>
-      
         </div>
       </div>
     </Layout>
