c25d27efaa9991aec103d502a8330d45ab1f0a40
Kashish Shah <kashish@lagrange.dev>
2025-02-11 14:31:12 +0530

    chore: fix images in docs (#51)

--- src/components/ThemedImage.tsx
+++ src/components/ThemedImage.tsx
@@ -5,17 +5,25 @@ interface ThemedImageProps {
   lightSrc: string;
   darkSrc: string;
   alt: string;
+  centered?: boolean;
 }
 
 const ThemedImage: React.FC<ThemedImageProps> = ({
   lightSrc,
   darkSrc,
   alt,
+  centered = true,
 }) => {
   const { colorMode } = useColorMode();
   const src = colorMode === "dark" ? darkSrc : lightSrc;
 
-  return <img src={src} alt={alt} />;
+  return (
+    <img
+      src={src}
+      alt={alt}
+      style={centered ? { display: "block", margin: "0 auto" } : undefined}
+    />
+  );
 };
 
 export default ThemedImage;
